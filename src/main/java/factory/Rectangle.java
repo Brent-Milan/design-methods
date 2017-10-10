d41ceeef59843136ef27143394a5b347c63a57c7
Brent-Milan <Bmilan24@gmail.com>
2017-10-10 14:14:46 -0400

    interface and initial class constructed

---
+++ src/main/java/factory/Rectangle.java
@@ -0,0 +1,9 @@
+package factory;
+
+public class Rectangle implements Shape {
+
+	@Override
+	public void draw() {
+		System.out.println("Console output for shape.draw() in class Rectangle.");
+	}
+}

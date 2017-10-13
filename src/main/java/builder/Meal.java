f4d4892e0ef29c47aaee9fae0b1ddbc3c654773a
Brent-Milan <Bmilan24@gmail.com>
2017-10-13 09:04:51 -0400

    Meal class created

---
+++ src/main/java/builder/Meal.java
@@ -0,0 +1,30 @@
+package builder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Meal {
+
+	private List<Item> items = new ArrayList<Item>();
+	
+	public void addItem(Item item) {
+		items.add(item);
+	}
+	
+	public float getCost() {
+		float cost = 0.0f;
+		
+		for(Item item: items) {
+			cost += item.price();
+		}
+		return cost;
+	}
+	
+	public void displayItems() {
+		for(Item item: items) {
+			System.out.println("Item: " + item.name());
+			System.out.println("Packaging: " + item.packaging());
+			System.out.println("Price: " + item.price());
+		}
+	}
+}

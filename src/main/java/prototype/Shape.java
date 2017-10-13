f3b208c11c93dca37ce0b4d264269fdb45c2d083
Brent-Milan <Bmilan24@gmail.com>
2017-10-13 15:58:14 -0400

    bringing current

--- src/main/java/prototype/Shape.java
+++ src/main/java/prototype/Shape.java
@@ -1,5 +1,32 @@
 package prototype;
 
-public enum Shape {
-
+public abstract class Shape {
+	
+	private String id;
+	protected String type;
+	
+	abstract void draw();
+	
+	public String getType() {
+		return type;
+	}
+	
+	public String getID() {
+		return id;
+	}
+	
+//	public void setId(String id) {
+//		this.id = id;
+//	}
+	
+	public Object clone() {
+		Object clone = null;
+		
+		try {
+			clone = super.clone();
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		return clone;
+	}
 }
